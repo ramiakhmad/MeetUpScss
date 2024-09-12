7fe955f39e708171bbe8541744bd93ae3b600d47
Rami Akhmad <rami.akhmad@gmail.com>
2024-09-12 19:05:24 +0200

    buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,18 @@
 const modal = document.querySelector(".modal");
 const singup = document.querySelector("#btn-modal");
 const singupClsoe = document.querySelector("#btn-close");
+const joinSingup = document.querySelector("#joinSingup");
+const joinSingupMobile = document.querySelector("#joinSingupMobile");
 
 singupClsoe.addEventListener("click", () => {
-    modal.classList.remove("modal-active");
-  });
+  modal.classList.remove("modal-active");
+});
 singup.addEventListener("click", () => {
   modal.classList.add("modal-active");
 });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector('#btn-open');
-    if (button) {
-        button.addEventListener('click', function() {
-            console.log('Button clicked!');
-        });
-    }
+joinSingup.addEventListener("click", () => {
+  modal.classList.add("modal-active");
 });
-
+joinSingupMobile.addEventListener("click", () => {
+  modal.classList.add("modal-active");
+});
